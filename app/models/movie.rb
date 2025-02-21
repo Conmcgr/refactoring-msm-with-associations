ff8ca784ec483f444f1b88524ae086ba75c2c7ba
Connor McGraw <42752783+Conmcgr@users.noreply.github.com>
2025-02-21 05:33:11 +0000

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,4 +15,10 @@
 class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
+
+  belongs_to(:director, foreign_key: "director_id", class_name: "Director")
+
+  has_many :characters
+
+  has_many :cast, through: :characters, source: :actor
 end
